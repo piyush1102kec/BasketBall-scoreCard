d9ae3e2ffb56af71391b76537f9e9f2a28d32d8c
Piyush Bhardwaj <piyush.b@acelsolutions.com>
2025-11-06 14:59:05 +0530

    added-all-three-files

---
+++ project.js
@@ -0,0 +1,34 @@
+let homePts = document.getElementById("home-pts")
+let homeCount = 0
+
+let guestPts = document.getElementById("guest-pts")
+
+let guestCount = 0
+
+function add1(){
+    homeCount += 1
+    homePts.textContent = homeCount
+}
+
+function add2(){
+    homeCount +=2
+    homePts.textContent = homeCount
+}
+function add3(){
+    homeCount += 3
+    homePts.textContent = homeCount
+}
+
+function inc1(){
+    guestCount += 1
+    guestPts.textContent = guestCount
+}
+
+function inc2(){
+    guestCount += 2
+    guestPts.textContent = guestCount
+}
+function inc3(){
+    guestCount += 3
+    guestPts.textContent = guestCount
+}
